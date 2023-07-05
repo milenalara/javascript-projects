263cb96fba3a1ced100602a27884f9e3aca452e6
Milena Lara <milenalrf@gmail.com>
2023-07-04 22:19:04 -0300

    added a loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// show loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete(){
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
@@ -17,15 +31,18 @@ async function getQuotes() {
     // handle error
     alert(error);
   }
+  complete();
 }
 
 // show new quote
 function newQuote(){
+  loading();
   let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   quote.author === 'Anonymous' ? authorText.textContent = 'Unknown': authorText.textContent = quote.author;
   // texts bigger than 50 characters get a smaller font-sinze
   quote.text.length > 50 ? quoteText.classList.add("long-quote") : quoteText.classList.remove("long-quote");
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // tweet a quote
